c73258a836845f021b1dedf3bb26ccbff1bf5f71
IloveSC228 <97229169+IloveSC228@users.noreply.github.com>
2024-08-15 17:09:36 +0800

    Add files via upload

---
+++ base/base_crawler.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+
+from playwright.async_api import BrowserContext, BrowserType
+
+
+class AbstractCrawler(ABC):
+    @abstractmethod
+    async def start(self):
+        pass
+
+    @abstractmethod
+    async def search(self):
+        pass
+
+    @abstractmethod
+    async def launch_browser(self, chromium: BrowserType, playwright_proxy: Optional[Dict], user_agent: Optional[str],
+                             headless: bool = True) -> BrowserContext:
+        pass
+
+
+class AbstractLogin(ABC):
+    @abstractmethod
+    async def begin(self):
+        pass
+
+    @abstractmethod
+    async def login_by_qrcode(self):
+        pass
+
+    @abstractmethod
+    async def login_by_mobile(self):
+        pass
+
+    @abstractmethod
+    async def login_by_cookies(self):
+        pass
+
+
+class AbstractStore(ABC):
+    @abstractmethod
+    async def store_hotlist(self, hotlist_item: Dict):
+        pass
+
+    @abstractmethod
+    async def store_content(self, content_item: Dict):
+        pass
+
+    @abstractmethod
+    async def store_comment(self, comment_item: Dict):
+        pass
+    
+    @abstractmethod
+    async def store_extract_info(self, extract_info_item: Dict):
+        pass
+
+    @abstractmethod
+    async def store_pic(self, pic_item):
+        pass
+    
+    @abstractmethod 
+    async def store_video(self, video_item):
+        pass
+
+class AbstractStoreImage(ABC):
+    # TODO: support all platform
+    # only weibo is supported
+    # @abstractmethod
+    async def store_image(self, image_content_item: Dict):
+        pass
+
+
+class AbstractApiClient(ABC):
+    @abstractmethod
+    async def request(self, method, url, **kwargs):
+        pass
+
+    @abstractmethod
+    async def update_cookies(self, browser_context: BrowserContext):
+        pass
